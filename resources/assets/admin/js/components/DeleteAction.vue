d9873f7feff97c4b7e800c3bb37ed90218e1c0a9
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-08-08 21:46:01 -0300

    Componente Modal de exclusão de banks

--- resources/assets/admin/js/components/DeleteAction.vue
+++ resources/assets/admin/js/components/DeleteAction.vue
@@ -5,8 +5,10 @@
         <input type="hidden" name="_token" :value="csrfToken"/>
     </form>
 </template>
+
 <script type="text/javascript">
     export default{
+        // Definição de propriedades
         props: ['action','csrfToken','actionElement'],
         computed: {
             id(){
@@ -15,17 +17,20 @@
         },
         ready(){
             let actionElement = this.actionElement,
-                formId = this.id;
+                       formId = this.id;
+
             $(document).ready(() => {
                 $(`#${actionElement}`).click((event) => {
-                    event.preventDefault();
+                    event.preventDefault(); // Cancela o evento do elemento
                     $(`#${formId}`).submit();
                 })
             })
         }
     }
 </script>
+
 <style scoped>
+    /* scoped aplica somente no escopo do formulário */
     form{
         display: none;
     }
--- resources/views/admin/banks/index.blade.php
+++ resources/views/admin/banks/index.blade.php
@@ -32,22 +32,22 @@
                     <td>{{ $bank->name }}</td>
                     <td>
                         <a href="{{ route('admin.banks.edit', ['bank' => $bank->id]) }}" class="btn waves-effect">Editar</a>
-                        <delete-action action="{{route('admin.banks.destroy',['bank'=>$bank->id])}}" action-element="link-delete-{{$bank->id}}" csrf-token="{{csrf_token()}}">
+                        <delete-action action="{{ route('admin.banks.destroy',['bank'=>$bank->id]) }}" action-element="link-delete-{{ $bank->id }}" csrf-token="{{csrf_token()}}">
                             <?php
                                 $modalId = "modal-delete-$bank->id";
                                 $deleteRoute = route('admin.banks.destroy',['bank'=> $bank->id]);
                             ?>
-                            <a id="link-modal-{{$bank->id}}" href="#{{$modalId}}" class="btn waves-effect">Excluir</a>
-                                <modal :modal="{{json_encode(['id'=>$modalId])}}" style="display: none">
+                            <a id="link-modal-{{ $bank->id}}" href="#{{ $modalId }}" class="btn waves-effect">Excluir</a>
+                                <modal :modal="{{ json_encode(['id'=>$modalId]) }}" style="display: none">
                                     <div slot="content">
                                         <h5>Mensagem de confirmação</h5>
                                         <p><strong>Deseja excluir esse banco?</strong></p>
                                         <div class="divider"></div>
-                                        <p>Nome: <strong>{{$bank->name}}</strong></p>
+                                        <p>Nome: <strong>{{ $bank->name }}</strong></p>
                                         <div class="divider"></div>
                                     </div>
                                     <div slot="footer">
-                                        <button id="link-delete-{{$bank->id}}" class="btn waves-effect modal-close modal-action">Ok</button>
+                                        <button id="link-delete-{{ $bank->id }}" class="btn waves-effect modal-close modal-action">Ok</button>
                                         <button class="btn btn-flat waves-effect waves-red modal-close modal-action">Cancelar</button>
                                     </div>
                                 </modal>
